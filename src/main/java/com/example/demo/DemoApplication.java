55eabdec589ed33d84ed426e1ca5e5b496b8fe1c
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2025-06-06 13:17:02 +0200

    feat: change 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    // change 1
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
